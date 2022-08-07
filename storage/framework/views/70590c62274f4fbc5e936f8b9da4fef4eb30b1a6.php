1a4a42a6a02a3da58ead0c4e513fa008e8fd49c5
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-08-08 01:47:32 +0500

    upload

---
+++ storage/framework/views/70590c62274f4fbc5e936f8b9da4fef4eb30b1a6.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <?php echo e(date('Y')); ?> © HMS
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-right d-none d-sm-block">
+                    <?php echo e(__("Design & Develop by Fyp Team")); ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php /**PATH C:\xampp\htdocs\health_care\health_care\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
