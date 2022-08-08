67d919ebe29a575330fb059d32c8dc31686f7b35
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-08-08 06:17:04 +0500

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,31 +12,13 @@
             <div class="row justify-content-center">
                 <div class="col-md-8 col-lg-6 col-xl-5">
                     <div class="card overflow-hidden">
-                        <div class="bg-soft-primary">
-                            <div class="row">
-                                <div class="col-7">
-                                    <div class="text-primary p-4">
-                                        <h5 class="text-primary">{{ __("Patient Register") }}</h5>
-                                        <p>Get your free {{ config('app.name'); }} Patient account now.</p>
-                                    </div>
-                                </div>
-                                <div class="col-5 align-self-end">
-                                    <img src="{{ URL::asset('assets/images/profile-img.png') }}" alt=""
-                                        class="img-fluid">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-body pt-0">
-                            <div>
+                        <div class="card-body">
+                            <div class="text-center">
                                 <a href="{{ url('/') }}">
-                                    <div class="avatar-md profile-user-wid mb-4">
-                                        <span class="avatar-title rounded-circle bg-light">
-                                            <img src="{{ URL::asset('assets/images/logo.png') }}" alt=""
-                                                class="rounded-circle" height="34">
-                                        </span>
-                                    </div>
+                                    <img src="{{ URL::asset('assets/images/logo.png') }}" alt="" class="rounded-circle" height="34">
                                 </a>
                             </div>
+                            <h2 class="text-center">Patient Register</h2>
                             <div class="p-2">
                                 <form method="POST" class="form-horizontal mt-4" action="{{ url('register') }}">
                                     @csrf
@@ -123,17 +105,13 @@
                                         <button class="btn btn-primary btn-block waves-effect waves-light"
                                             type="submit">{{ __("Register") }}</button>
                                     </div>
-                                    <div class="mt-4 text-center">
-                                        <p class="mb-0">By registering you agree to the {{ config('app.name'); }} <a href="#" class="text-primary">{{ __("Terms of Use") }}</a></p>
-                                    </div>
                                 </form>
                             </div>
+                            <hr/>
+                            <div class="text-center">
+                                {{ __("Already have an account ?") }} <a href="{{ url('login') }}" class="font-weight-medium text-primary">{{ __("Login") }} </a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="mt-5 text-center">
-                        <p>{{ __("Already have an account ?") }} <a href="{{ url('login') }}"
-                                class="font-weight-medium text-primary">{{ __("Login") }} </a> </p>
-                        <p>© {{ date('Y') }} {{ config('app.name'); }}. Crafted with <i class="mdi mdi-heart text-danger"></i> {{ __("by Themesbrand") }}</p>
                     </div>
                 </div>
             </div>
