a0feee0b7e7e1b011bf13027c279541009b9daa2
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-08-09 02:52:35 +0500

    Update view-prescription.blade.php

--- resources/views/prescription/view-prescription.blade.php
+++ resources/views/prescription/view-prescription.blade.php
@@ -16,9 +16,8 @@
         <div class="row d-print-none">
             <div class="col-12">
                 <a href="{{ url('prescription') }}">
-                    <button type="button" class="btn btn-primary waves-effect waves-light mb-4">
-                        <i
-                            class="bx bx-arrow-back font-size-16 align-middle mr-2"></i>{{ __('Back to Prescription List') }}
+                    <button type="button" class="btn btn-success waves-effect waves-light mb-4">
+                        <i class="bx bx-arrow-back font-size-16 align-middle mr-2"></i>{{ __('Back to Prescription List') }}
                     </button>
                 </a>
                 <a href="javascript:window.print()" class="btn btn-success waves-effect waves-light mb-4">
@@ -81,8 +80,8 @@
                                     <h3 class="font-size-15 font-weight-bold">{{ __('Medications') }}</h3>
                                 </div>
                                 <div class="table-responsive">
-                                    <table class="table">
-                                        <thead>
+                                    <table class="table table-centered table-nowrap mb-0 table-striped">
+                                        <thead class="bg-success text-dark">
                                             <tr>
                                                 <th style="width: 70px;">{{ __('No.') }}</th>
                                                 <th>{{ __('Name') }}</th>
@@ -106,8 +105,8 @@
                                     <h3 class="font-size-15 font-weight-bold">{{ __('Test Reports') }}</h3>
                                 </div>
                                 <div class="table-responsive">
-                                    <table class="table">
-                                        <thead>
+                                    <table class="table table-centered table-nowrap mb-0 table-striped">
+                                        <thead class="bg-success text-dark">
                                             <tr>
                                                 <th style="width: 70px;">{{ __('No.') }}</th>
                                                 <th>{{ __('Name') }}</th>
