7895c273956fb33f7acafc9ce61ec73c41d9d97b
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-08-11 01:25:15 +0500

    deleted duplicate files

--- resources/resources temp/views/appointment/ajax-select-appointment.blade.php
+++
@@ -1,6 +0,0 @@
-<option selected value="">--- Select Appointment ---</option>
-@if (!empty($appointment))
-    @foreach ($appointment as $key => $value)
-        <option value="{{ $key }}">{{ $value }}</option>
-    @endforeach
-@endif
