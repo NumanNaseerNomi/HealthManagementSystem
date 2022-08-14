67cbd44132b33c179217759fad3628baacd37340
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-08-14 19:19:53 +0500

    Create MessagesController.php

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+
+class MessagesController extends Controller
+{
+    function chats()
+    {
+        $user = Sentinel::getUser();
+        
+        if ($user->hasAccess('patient.create'))
+        {
+            $role = $user->roles[0]->slug;
+            $patient = null;
+            $patient_info = null;
+            $medical_info = null;
+            return view('messages', compact('user', 'role', 'patient', 'patient_info', 'medical_info'));
+        }
+        else
+        {
+            return redirect('login')->with('error','Please Login');
+        }
+    }
+}
