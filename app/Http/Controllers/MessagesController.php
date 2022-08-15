81becf8bbb822f39352522758326f72f06618e77
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-08-16 04:15:08 +0500

    Update MessagesController.php

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+use App\MessagesModel;
 
 class MessagesController extends Controller
 {
     function chats()
     {
+        // dd(MessagesModel::all());
         $user = Sentinel::getUser();
+        // dd($user);
 
         if ($user)
         {
@@ -17,11 +20,27 @@ class MessagesController extends Controller
             $patient = null;
             $patient_info = null;
             $medical_info = null;
-            return view('messages', compact('user', 'role', 'patient', 'patient_info', 'medical_info'));
+            $chatUsers = MessagesModel::where("from", $user->id)->get()->unique('to');
+            // dd($chatUsers);
+
+            return view('messages', compact('user', 'role', 'patient', 'patient_info', 'medical_info', "chatUsers"));
         }
         else
         {
             return redirect('login')->with('error','Please Login');
         }
+    }
+
+    function getChatUsers()
+    {
+        $user = Sentinel::getUser();
+        $chatUsers = MessagesModel::where("from", $user->id)->get();
+        dd($chatUsers);
+        // dd($user->id);
+    }
+
+    function sendMessage(Request $request)
+    {
+        dd($request);
     }
 }
