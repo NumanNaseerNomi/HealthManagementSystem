1b5e3fcb4044973a7b269aa8af5175c5e61ac727
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-09-25 03:21:17 +0500

    Update TestReportController.php

--- app/Http/Controllers/TestReportController.php
+++ app/Http/Controllers/TestReportController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TestReport;
 
 class TestReportController extends Controller
 {
-    //
+    function update(Request $request)
+    {
+        // dd($request->result);
+        TestReport::where("id", 1)->update(["result" => $request->result]);
+    }
 }
