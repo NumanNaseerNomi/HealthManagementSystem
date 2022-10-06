bc4caa6f035b57654679d59e9585a82ce08d807f
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-07 01:50:10 +0500

    Update doctor-profile-view.blade.php

--- resources/views/doctor/doctor-profile-view.blade.php
+++ resources/views/doctor/doctor-profile-view.blade.php
@@ -16,51 +16,6 @@
         @endcomponent
         <div class="row">
             <div class="col-xl-4">
-                <!-- <div class="card overflow-hidden">
-                    <div class="bg-soft-primary">
-                        <div class="row">
-                            <div class="col-7">
-                                <div class="text-primary p-3">
-                                    <h5 class="text-primary">{{ __('Doctor Information') }}</h5>
-                                </div>
-                            </div>
-                            <div class="col-5 align-self-end">
-                                <img src="{{ URL::asset('assets/images/profile-img.png') }}" alt="" class="img-fluid">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body pt-0">
-                        <div class="row">
-                            <div class="col-sm-4">
-                                <div class="avatar-md profile-user-wid mb-4">
-                                    <img src="@if ($doctor->profile_photo != ''){{ URL::asset('storage/images/users/' . $doctor->profile_photo) }}@else{{ URL::asset('assets/images/users/noImage.png') }}@endif" alt="{{ $doctor->first_name }}"
-                                        class="img-thumbnail rounded-circle">
-                                </div>
-                                <h5 class="font-size-15 text-truncate"> {{ $doctor->first_name }}
-                                    {{ $doctor->last_name }} </h5>
-                                <p class="text-muted mb-0 text-truncate"> {{ $doctor_info->title }} </p>
-                            </div>
-                            <div class="col-sm-8">
-                                <div class="pt-4">
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <h5 class="font-size-12">{{ __('Last Login :') }} </h5>
-                                            <p class="text-muted mb-0"> {{ $doctor->last_login }} </p>
-                                        </div>
-                                    </div>
-                                    <div class="mt-4">
-                                        <a href="{{ url('profile-edit') }}"
-                                            class="btn btn-primary waves-effect waves-light btn-sm">{{ __('Edit Profile') }}
-                                            <i class="mdi mdi-arrow-right ml-1"></i></a>
-                                        <a href="{{ url('time-edit/' . $doctor->id) }}"
-                                            class="btn btn-primary waves-effect waves-light btn-sm">{{ __('Edit Time Slot') }}
-                                            <i class="mdi mdi-arrow-right ml-1"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
                 <!-- end card -->
                 <div class="card">
                     <div class="card-body">
