fb9c75451417e7f280be455c5e9e7aa6bc77257e
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-07 01:33:39 +0500

    Update doctor-profile.blade.php

--- resources/views/doctor/doctor-profile.blade.php
+++ resources/views/doctor/doctor-profile.blade.php
@@ -217,12 +217,12 @@
                                     <span class="d-none d-sm-block">{{ __('Appointment List') }}</span>
                                 </a>
                             </li>
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <a class="nav-link" data-toggle="tab" href="#PrescriptionList" role="tab">
                                     <span class="d-block d-sm-none"><i class="far fa-envelope"></i></span>
                                     <span class="d-none d-sm-block">{{ __('Prescription List') }}</span>
                                 </a>
-                            </li>
+                            </li> -->
                             <li class="nav-item">
                                 <a class="nav-link" data-toggle="tab" href="#Invoices" role="tab">
                                     <span class="d-block d-sm-none"><i class="fas fa-cog"></i></span>
