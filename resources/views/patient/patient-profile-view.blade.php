ab73318c4b9e4b116f4bbb8df1db513fd045feb7
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-07 01:57:29 +0500

    Update patient-profile-view.blade.php

--- resources/views/patient/patient-profile-view.blade.php
+++ resources/views/patient/patient-profile-view.blade.php
@@ -71,32 +71,6 @@
                             </div>
                         </div>
                     </div>
-                    <!-- <div class="col-md-4">
-                        <div class="card mini-stats-wid">
-                            <div class="card-body">
-                                <div class="media">
-                                    <div class="media-body">
-                                        <p class="text-muted font-weight-medium">{{ __('Pending Bills') }}</p>
-                                        <h4 class="mb-0">{{ number_format($data['pending_bill']) }}</h4>
-                                    </div>
-                                    <i class="bx bx-hourglass font-size-24 text-success"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card mini-stats-wid">
-                            <div class="card-body">
-                                <div class="media">
-                                    <div class="media-body">
-                                        <p class="text-muted font-weight-medium">{{ __('Total Bill') }}</p>
-                                        <h4 class="mb-0">${{ number_format($data['revenue'], 2) }}</h4>
-                                    </div>
-                                    <i class="bx bx-package font-size-24 text-success"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
                 </div>
                 <div class="card">
                     <div class="card-body">
